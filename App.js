e64e18fe32377d9863900526e3be08c647dca121
annaantonn <105316891+annaantonn@users.noreply.github.com>
2023-03-08 14:47:16 -0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Mood from "./Mood.js";
+import data from "./data.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Mood />
+    </div>
+  );
+}
